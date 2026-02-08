484b55d13c6106576d448bcc7186d6844c00e167
denialtorres <denial.torres@gmail.com>
2026-02-08 00:40:24 -0600

    Add QuickBooks Online OAuth integration

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,8 +1,32 @@
 class AccountsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [ :oauth2_callback ]
+  include OauthActions
 
   def index
     @user = current_user
     @account = @user&.account
+  end
+
+  def qbo_account
+    set_oauth2_state
+    @redirect_uri = Qbo::Base.oauth2_client.authorization_uri(scope: "com.intuit.quickbooks.accounting",
+      state: session[:oauth2_state])
+    @qbo_account = current_account.qbo_account || current_account.build_qbo_account
+    # @form = QboAccountForm.new(@qbo_account)
+    if request.patch?
+      @form.submit(qbo_account_params)
+    end
+    # @items = current_account.items
+  end
+
+  private
+
+  # The session[:oauth2_state] is used to protect against a CSRF. When testing use a static uuid and not a random one.”
+  def set_oauth2_state
+    session[:oauth2_state] = SecureRandom.hex(16)
+  end
+
+  def current_account
+    @current_account ||= current_user.account
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < Devise::SessionsController
   include Devise::Controllers::Rememberable
   def after_sign_in_path_for(resource_or_scope)
     if resource_or_scope.is_a?(User) && resource_or_scope.account.present?
-      accounts_index_path  # Users with accounts → dashboard
+      accounts_path  # Users with accounts → dashboard
     else
       root_path  # Users without accounts (like admin@example.com) → home
     end
